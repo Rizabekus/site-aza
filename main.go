e2d9e272213ea74acbaeb152a56e1026e8cea34b
azakonov <zakonovalikhan@gmail.com>
2024-04-01 02:38:51 +0500

    todo footer

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("1")
 	mux := http.NewServeMux()
+	fmt.Println("12")
 	files := http.FileServer(http.Dir("./static"))
+	fmt.Println("13")
 	mux.Handle("/static/", http.StripPrefix("/static", files))
-
+	fmt.Println("14")
 	mux.HandleFunc("/", Homepage)
+	fmt.Println("15")
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		fmt.Println("gg")
+		log.Fatal(err)
+	}
+	fmt.Println("??")
+	fmt.Println("http://localhost:8000")
 }
 
 func Homepage(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +37,5 @@ func Homepage(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(http.StatusText(http.StatusBadRequest)))
 		return
 	}
+	http.ServeFile(w, r, "./static/home.html")
 }
